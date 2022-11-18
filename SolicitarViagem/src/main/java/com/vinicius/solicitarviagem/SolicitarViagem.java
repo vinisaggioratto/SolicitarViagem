3cf9518761e8465be83968e96d9b2e0cdc0dbe51
Vinicius Gabriel Saggioratto <viniciussaggioratto@gmail.com>
2022-11-18 20:57:13 -0300

    Substituido o projeto original, pois apresentou conflito na inicialização.

--- SolicitarViagem/src/main/java/com/vinicius/solicitarviagem/SolicitarViagem.java
+++ SolicitarViagem/src/main/java/com/vinicius/solicitarviagem/SolicitarViagem.java
@@ -1,7 +1,14 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
 
 package com.vinicius.solicitarviagem;
 
+/**
+ *
+ * @author vinicius-dev
+ */
 public class SolicitarViagem {
 
     public static void main(String[] args) {
